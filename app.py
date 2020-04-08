98186b5b44931c8d6c2122238e8f9f293125601e
jfsystemsuk <jfsystemsuk@yahoo.co.uk>
2020-04-08 12:31:33 +0000

    app.py updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-y0jyu.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
@@ -14,6 +14,11 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
 
 
 if __name__ == '__main__':
